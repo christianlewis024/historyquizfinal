bd1e1b0356e3520890834b15b558792ddf846c3d
christianlewis024 <58643162+christianlewis024@users.noreply.github.com>
2020-02-17 17:17:53 -0800

    updated for week 4
    week 4 updated questions on thomas jefferson, updated scores,  updated study tab for current quiz+ next weeks topic of declaration and new york campaign. 2/17/19

--- auth.js
+++ auth.js
@@ -1,66 +1,70 @@
-
-
 //listen for auth status changes
 auth.onAuthStateChanged(user => {
-    console.log(user)
-    if (user) {
-        db.collection('guides').onSnapshot(snapshot => {
-            setupUI(user);
-        }, err => console.log(err.message));
-    } else {
-        setupUI();
-
-    }
+  console.log(user);
+  if (user) {
+    db.collection("guides").onSnapshot(
+      snapshot => {
+        setupUI(user);
+      },
+      err => console.log(err.message)
+    );
+  } else {
+    setupUI();
+  }
 });
 
 // signup
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit", e => {
+  e.preventDefault();
 
-    // get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
+  // get user info
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
 
-    // sign up the user & add firestore data
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        return db.collection('users').doc(cred.user.uid).set({
-            bio: signupForm['signup-bio'].value,
-            username: signupForm['username'].value,
-            email: signupForm['signup-email'].value
+  // sign up the user & add firestore data
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(cred => {
+      return db
+        .collection("users")
+        .doc(cred.user.uid)
+        .set({
+          bio: signupForm["signup-bio"].value,
+          username: signupForm["username"].value,
+          email: signupForm["signup-email"].value
         });
-
-    }).then(() => {
-        // close the signup modal & reset form
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
-        signupForm.reset();
+    })
+    .then(() => {
+      // close the signup modal & reset form
+      const modal = document.querySelector("#modal-signup");
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
     });
 });
 // logout
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    auth.signOut();
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", e => {
+  e.preventDefault();
+  auth.signOut();
 });
 
 // login
-const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", e => {
+  e.preventDefault();
 
-    // get user info
-    const email = loginForm['login-email'].value;
-    const password = loginForm['login-password'].value;
+  // get user info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
 
-    // log the user in
-    auth.signInWithEmailAndPassword(email, password).then((cred) => {
-        // close the signup modal & reset form
-        const modal = document.querySelector('#modal-login');
-        M.Modal.getInstance(modal).close();
-        loginForm.reset();
-    });
-
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    // close the signup modal & reset form
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
 });
 
 // const auth = require("registry-auth-token");
@@ -68,7 +72,6 @@ loginForm.addEventListener('submit', (e) => {
 // Testing below //
 // Testing below //
 // Testing below //
-
 
 // var user = firebase.auth().currentUser;
 // var name, email;
@@ -86,24 +89,22 @@ loginForm.addEventListener('submit', (e) => {
 //     }
 // }
 
-
-
 function submitScore() {
-    var user = firebase.auth().currentUser;
-    var name, email;
+  var user = firebase.auth().currentUser;
+  var name, email;
 
-    if (user) {
-        name = user.username;
-        email = user.email;
+  if (user) {
+    name = user.username;
+    email = user.email;
 
-        db.collection("scores3").doc().set({
-            score: score,
-            name: name || email
-
-        })
-    }
+    db.collection("scores3")
+      .doc()
+      .set({
+        score: score,
+        name: name || email
+      });
+  }
 }
-
 
 // // select all elements
 
@@ -129,8 +130,6 @@ const scoreDiv = document.getElementById("scoreContainer");
 
 // // from discord //
 
-
-
 // const currentUser = document.getElementsByName("username");
 // const getUsername = document.querySelector('')
 // const user = auth.currentUser;
@@ -154,8 +153,6 @@ const scoreDiv = document.getElementById("scoreContainer");
 
 //     }
 // })
-
-
 
 // function submitScore() {
 //     db.collection("scores").doc().set({
@@ -232,88 +229,109 @@ const scoreDiv = document.getElementById("scoreContainer");
 // });
 // create our questions
 let questions = [
-    {   // QUESTION #1
-        question: "Who took command of British Forces in North America directly after Thomas Gage?",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "Lord North",
-        choiceB: "William Howe",
-        choiceC: "Richard Howe",
-        choiceD: "Benjamin Lincoln",
-        correct: "B"
-    }, { // QUESTION #2
-        question: "Where did most of the fighting of Bunker Hill actually take place?",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "Bunker Hill",
-        choiceB: "Nob Hill",
-        choiceC: "Hank's Hill",
-        choiceD: "Breed's Hill",
-        correct: "D"
-    },
-    {   // QUESTION #3
-        question: "How many attempts did it take for the British to take Breed's Hill?",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "1",
-        choiceB: "2",
-        choiceC: "3",
-        choiceD: "4",
-        correct: "C"
-    }, { // QUESTION #4
-        question: "Which side suffered greater casualties in the battle of Bunker Hill?",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "British",
-        choiceB: "American",
-        choiceC: "Even",
-        choiceD: "French",
-        correct: "A"
-    }, { // QUESTION #5
-        question: "Who's quote is this? 'I wish we could sell them another hill at the same price'",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "Nathanael Greene",
-        choiceB: "George Washington",
-        choiceC: "Israel Putnam",
-        choiceD: "George III",
-        correct: "A"
-    }, { // QUESTION #6
-        question: "Which British controlled fort did Benedict Arnold and Ethan Allen take?",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "Fort Knox",
-        choiceB: "Fort McHenry ",
-        choiceC: "Fort Sumter ",
-        choiceD: "Fort Ticonderoga",
-        correct: "D"
-    }, { // QUESTION #7
-        question: "Which lake is Fort Ticonderoga nearest?",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "Lake Erie",
-        choiceB: "Lake Champlain",
-        choiceC: "Lake Ticonderoga ",
-        choiceD: "Lake Mead",
-        correct: "B"
-    }, { // QUESTION #8
-        question: "In the Quebec campaign, who was the British general in charge in Canada?",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "Michael Bolton",
-        choiceB: "Banastre Tarleton",
-        choiceC: "Guy Carleton",
-        choiceD: "Charles Stanton",
-        correct: "C"
-    }, { // QUESTION #9
-        question: "Which American general attacked and occupied Montreal?",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "Richard Montgomery",
-        choiceB: "Henry Knox",
-        choiceC: "Nathanael Greene",
-        choiceD: "Friedrich Wilhelm von Steuben",
-        correct: "A"
-    }, { // QUESTION #10
-        question: "What injury did Benedict Arnold recieve in the Quebec campaign?",
-        imgSrc: "img/bunkerhill.jpg",
-        choiceA: "Amputated feet from frostbite",
-        choiceB: "Stabbed in left eye",
-        choiceC: "Hanged to death",
-        choiceD: "Shot in the leg",
-        correct: "D"
-    }
+  {
+    // QUESTION #1
+    question: "Who was Thomas Jefferson's childhood best friend?",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "George Washington",
+    choiceB: "John Adams",
+    choiceC: "The Marquis de Lafayette",
+    choiceD: "Dabney Carr",
+    correct: "D"
+  },
+  {
+    // QUESTION #2
+    question: "What was Thomas Jefferson's wife's name?",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "Louisa",
+    choiceB: "Martha",
+    choiceC: "Dolly",
+    choiceD: "Elizabeth",
+    correct: "B"
+  },
+  {
+    // QUESTION #3
+    question: "Which college did Jefferson graduate from?",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "Yale",
+    choiceB: "Harvard",
+    choiceC: "The College of William and Mary",
+    choiceD: "St. John's College",
+    correct: "C"
+  },
+  {
+    // QUESTION #4
+    question:
+      "In Virginia, June 1774, To show solidarity with other colonies and to protest the Boston Port Act, a day of ____ was declared ",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "Fasting",
+    choiceB: "Rioting",
+    choiceC: "Silence",
+    choiceD: "War preperation",
+    correct: "A"
+  },
+  {
+    // QUESTION #5
+    question: "In August 1774, Jefferson published  ___",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "The Declaration of Independence",
+    choiceB: "A Summary View of the Rights of British America",
+    choiceC: "Constitution of the United States",
+    choiceD: "Common Sense",
+    correct: "B"
+  },
+  {
+    // QUESTION #6
+    question:
+      "What year did Jefferson first get elected to Governor of Virginia?",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "1765",
+    choiceB: "1770",
+    choiceC: "1776",
+    choiceD: "1779",
+    correct: "D"
+  },
+  {
+    // QUESTION #7
+    question:
+      "In 1784, Jefferson becomes the forgein minister to which country?",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "France",
+    choiceB: "England",
+    choiceC: "Russia",
+    choiceD: "Spain",
+    correct: "A"
+  },
+  {
+    // QUESTION #8
+    question: "What was Jefferson's role in Washington's presidency?",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "Vice President",
+    choiceB: "Secretary of War",
+    choiceC: "Treasury Dept.",
+    choiceD: "Secretary of State",
+    correct: "D"
+  },
+  {
+    // QUESTION #9
+    question: "What was Jefferson's role in the Adams presidency?",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "Vice President",
+    choiceB: "Secretary of War",
+    choiceC: "Treasury Dept",
+    choiceD: "Secretary of State",
+    correct: "A"
+  },
+  {
+    // QUESTION #10
+    question: "What was the name of Thomas Jefferson's Home?",
+    imgSrc: "img/thomasjefferson.jpg",
+    choiceA: "Montecito",
+    choiceB: "Mount Vernon",
+    choiceC: "Monticello",
+    choiceD: "Montpelier",
+    correct: "C"
+  }
 ];
 
 // create some variables
@@ -329,121 +347,124 @@ let score = 0;
 
 // render a question
 function renderQuestion() {
-    let q = questions[runningQuestion];
+  let q = questions[runningQuestion];
 
-    question.innerHTML = "<p>" + q.question + "</p>";
-    qImg.innerHTML = "<img src=" + q.imgSrc + ">";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
+  question.innerHTML = "<p>" + q.question + "</p>";
+  qImg.innerHTML = "<img src=" + q.imgSrc + ">";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+  choiceD.innerHTML = q.choiceD;
 }
 
 start.addEventListener("click", startQuiz);
 
 // start quiz
 function startQuiz() {
-    start.style.display = "none";
-    renderQuestion();
-    quiz.style.display = "block";
-    renderProgress();
-    renderCounter();
-    TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
+  start.style.display = "none";
+  renderQuestion();
+  quiz.style.display = "block";
+  renderProgress();
+  renderCounter();
+  TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
 }
 
 // render progress
 function renderProgress() {
-    for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-        progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
-    }
+  for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+  }
 }
 
 // counter render
 
 function renderCounter() {
-    if (count <= questionTime) {
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
+  if (count <= questionTime) {
+    counter.innerHTML = count;
+    timeGauge.style.width = count * gaugeUnit + "px";
+    count++;
+  } else {
+    count = 0;
+    // change progress color to red
+    answerIsWrong();
+    if (runningQuestion < lastQuestion) {
+      runningQuestion++;
+      renderQuestion();
     } else {
-        count = 0;
-        // change progress color to red
-        answerIsWrong();
-        if (runningQuestion < lastQuestion) {
-            runningQuestion++;
-            renderQuestion();
-        } else {
-            // end the quiz and show the score
-            clearInterval(TIMER);
-            scoreRender();
-        }
+      // end the quiz and show the score
+      clearInterval(TIMER);
+      scoreRender();
     }
+  }
 }
 
 // checkAnwer
 
 function checkAnswer(answer) {
-    if (answer == questions[runningQuestion].correct) {
-        // answer is correct
-        score++;
-        // change progress color to green
-        answerIsCorrect();
-    } else {
-        // answer is wrong
-        // change progress color to red
-        answerIsWrong();
-    }
-    count = 0;
-    if (runningQuestion < lastQuestion) {
-        runningQuestion++;
-        renderQuestion();
-    } else {
-        // end the quiz and show the score
-        clearInterval(TIMER);
-        scoreRender();
-        submitScore();
-    }
+  if (answer == questions[runningQuestion].correct) {
+    // answer is correct
+    score++;
+    // change progress color to green
+    answerIsCorrect();
+  } else {
+    // answer is wrong
+    // change progress color to red
+    answerIsWrong();
+  }
+  count = 0;
+  if (runningQuestion < lastQuestion) {
+    runningQuestion++;
+    renderQuestion();
+  } else {
+    // end the quiz and show the score
+    clearInterval(TIMER);
+    scoreRender();
+    submitScore();
+  }
 }
 
 // answer is correct
 function answerIsCorrect() {
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+  document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
 }
 
 // answer is Wrong
 function answerIsWrong() {
-    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+  document.getElementById(runningQuestion).style.backgroundColor = "#f00";
 }
 
-// submit the score 
+// submit the score
 
 // function submitScore() {
 //     db.collection("scores").doc().set({
 //         score: score,
 //         name: accountDetails['username'].value
 
-
 //     })
 // }
 
 // score render
 function scoreRender() {
-    scoreDiv.style.display = "block";
+  scoreDiv.style.display = "block";
 
-    // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score / questions.length);
+  // calculate the amount of question percent answered by the user
+  const scorePerCent = Math.round((100 * score) / questions.length);
 
-    // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-        (scorePerCent >= 60) ? "img/4.png" :
-            (scorePerCent >= 40) ? "img/3.png" :
-                (scorePerCent >= 20) ? "img/2.png" :
-                    "img/1.png";
+  // choose the image based on the scorePerCent
+  let img =
+    scorePerCent >= 80
+      ? "img/5.png"
+      : scorePerCent >= 60
+      ? "img/4.png"
+      : scorePerCent >= 40
+      ? "img/3.png"
+      : scorePerCent >= 20
+      ? "img/2.png"
+      : "img/1.png";
 
-    scoreDiv.innerHTML = "<img src=" + img + ">";
-    scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
+  scoreDiv.innerHTML = "<img src=" + img + ">";
+  scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
 }
-
 
 // function saveScore() {
 //     // // Get name from input box
@@ -457,9 +478,7 @@ function scoreRender() {
 //             score: score
 //         })
 
-
 // ^ needs to close above
-
 
 // const createForm = document.querySelector('#create-form');
 // createForm.addEventListener('submit', (e) => {
@@ -468,19 +487,3 @@ function scoreRender() {
 //         title: createForm.title.value,
 //         content: createForm.content.value
 //     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
